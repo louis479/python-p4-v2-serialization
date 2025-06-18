b9807f348e377a0b9dd80e703de8d9d05e5fad5b
louis-ngugi <louisngugi858@gmail.com>
2025-06-18 16:12:41 +0300

    serialization

--- server/models.py
+++ server/models.py
@@ -11,14 +11,12 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     species = db.Column(db.String(50), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
 
     def to_dict(self):
         return {
             "id": self.id,
             "name": self.name,
             "species": self.species,
-            "age": self.age
         }
 
     def __repr__(self):
